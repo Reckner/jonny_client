0400a8f2ea45daac79f2d264ff52fd48b3954d7a
Paul Opmann <paul.opmann@gmail.com>
2020-05-02 21:25:44 +0300

    auth and sign in endpoints are being called

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,33 +1,83 @@
-import React from 'react';
+import React, { useState } from 'react';
 import $ from 'jquery';
 
 import './Hero.scss';
+import axios from 'axios';
 
 const Hero: React.FC = () => {
-    const changeButton = e => {
+    const [logEmail, setLogEmail] = useState<string>('');
+    const [logPassword, setLogPassword] = useState<string>('');
+
+    const [regEmail, setRegEmail] = useState<string>('');
+    const [regUserName, setRegUserName] = useState<string>('');
+    const [regPassword, setRegPassword] = useState<string>('');
+
+    const changeButton = (e) => {
         e.preventDefault();
         $('form').animate({ height: 'toggle', opacity: 'toggle' }, 'slow');
+    };
+
+    const handleSignIn = async (e) => {
+        e.preventDefault();
+
+        const response = await axios.post('http://127.0.0.1:5000/auth', {
+            email: logEmail,
+            password: logPassword,
+        });
+
+        console.log(response);
+    };
+
+    const handleCreateAccount = async (e) => {
+        e.preventDefault();
+
+        await axios.post('http://127.0.0.1:5000/user', {
+            email: regEmail,
+            username: regUserName,
+            password: regPassword,
+        });
     };
 
     return (
         <div className="login-page">
             <div className="form">
                 <h2>JONNY</h2>
-                <form className="register-form">
+                <form className="register-form" onSubmit={handleCreateAccount}>
                     <div className="input-wrapper">
-                        <input type="text" id="reg-name" required />
-                        <label htmlFor="reg-name">name</label>
-                    </div>
-                    <div className="input-wrapper">
-                        <input type="password" id="reg-password" required />
-                        <label htmlFor="reg-password">password</label>
-                    </div>
-                    <div className="input-wrapper">
-                        <input type="text" id="email" required />
+                        <input
+                            type="text"
+                            id="reg-email"
+                            required
+                            onChange={(e) => {
+                                setRegEmail(e.target.value);
+                            }}
+                        />
                         <label htmlFor="email">email</label>
                     </div>
+                    <div className="input-wrapper">
+                        <input
+                            type="text"
+                            id="reg-username"
+                            required
+                            onChange={(e) => {
+                                setRegUserName(e.target.value);
+                            }}
+                        />
+                        <label htmlFor="reg-name">username</label>
+                    </div>
+                    <div className="input-wrapper">
+                        <input
+                            type="password"
+                            id="reg-password"
+                            required
+                            onChange={(e) => {
+                                setRegPassword(e.target.value);
+                            }}
+                        />
+                        <label htmlFor="reg-password">password</label>
+                    </div>
 
-                    <button>create</button>
+                    <button type="submit">create</button>
                     <p className="message">
                         Already registered?{' '}
                         <a onClick={changeButton} href="#">
@@ -35,17 +85,32 @@ const Hero: React.FC = () => {
                         </a>
                     </p>
                 </form>
-                <form className="login-form">
+                <form className="login-form" onSubmit={handleSignIn}>
                     <div className="input-wrapper">
-                        <input type="text" id="log-name" required />
-                        <label htmlFor="log-name">name</label>
+                        <input
+                            type="text"
+                            id="log-email"
+                            required
+                            onChange={(e) => {
+                                setLogEmail(e.target.value);
+                            }}
+                        />
+                        <label htmlFor="log-name">email</label>
                     </div>
                     <div className="input-wrapper">
-                        <input type="password" id="log-password" required />
+                        <input
+                            type="password"
+                            id="log-password"
+                            required
+                            onChange={(e) => {
+                                setLogPassword(e.target.value);
+                            }}
+                        />
                         <label htmlFor="log-password">password</label>
                     </div>
 
                     <button>login</button>
+                    <button>fuck it</button>
                     <p className="message">
                         Not registered?{' '}
                         <a onClick={changeButton} href="#">
